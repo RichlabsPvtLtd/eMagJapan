bc71f7da4100d6436ea48fe231444388c7507d37
Ams <streetw@yahoo.com>
2024-05-12 06:07:39 -0400

    UI fixes

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -50,7 +50,7 @@ export const Carousel = ({ onLogout }) => {
       </div>
       <Slider {...settings}>
         <div className="imageWrapper">
-          <img src={require("./assets/ab6.png")} alt="Slide 1" />
+          <img src={require("./assets/ab1.png")} alt="Slide 1" />
         </div>
         <div className="imageWrapper">
           <img src={require("./assets/ab2.png")} alt="Slide 2" />
@@ -65,7 +65,7 @@ export const Carousel = ({ onLogout }) => {
           <img src={require("./assets/ab5.png")} alt="Slide 4" />
         </div>
         <div className="imageWrapper">
-          <img src={require("./assets/ab1.png")} alt="Slide 4" />
+          <img src={require("./assets/ab6.png")} alt="Slide 4" />
         </div>
       </Slider>
     </div>
